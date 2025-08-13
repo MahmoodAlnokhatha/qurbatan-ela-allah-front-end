405f0a48bb1c7a5be50e9d29de8143f26ab80c81
Mahmood AlNokhatha <mahmoodalnokhtha@gmail.com>
2025-08-13 20:31:09 +0300

    Delete src/Services directory

--- src/Services/authService.js
+++
@@ -1,5 +0,0 @@
-import http from './http';
-
-export const signup = (data) => http.post('/auth/sign-up', data);
-export const login  = (data) => http.post('/auth/sign-in', data);
-export const verify = () => http.get('/auth/verify');
