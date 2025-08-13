69efca4a51a09aa9cd9ab80530c08c643f9149f9
haddadoz89 <haddadoz1989@gmail.com>
2025-08-13 20:35:48 +0300

    change file name

--- src/Services/vehicleService.js
+++
@@ -1,8 +0,0 @@
-import http from './http';
-
-export const listVehicles   = () => http.get('/vehicles');
-export const createVehicle  = (formData) => http.post('/vehicles', formData);
-export const updateVehicle  = (id, data) => http.put(`/vehicles/${id}`, data);
-export const deleteVehicle  = (id) => http.delete(`/vehicles/${id}`);
-export const myVehicles     = () => http.get('/vehicles/my-vehicles');
-export const getVehicle     = (id) => http.get(`/vehicles/${id}`);
